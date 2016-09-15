15c4befff6ee227256a43b5e8759d32dea8dc7a0
Misha Mezhebovski <mezh.mish@gmail.com>
2016-09-15 16:22:19 +0300

    adding user input

--- 0SKETCH.py
+++ 0SKETCH.py
@@ -3,7 +3,7 @@ from sys import argv
 
 script, first, second, third = argv
 
-print "The script is called:", script
+print raw_input("The script is called:"), script
 print raw_input("trying to count seconds"), second
 print "Your second variable is:", first
 print "Your third variable is:", third
