77cb813f62bddc6c452910af151f328c1c60045a
Misha Mezhebovski <mezh.mish@gmail.com>
2016-10-10 17:41:06 +0300

    final implementation of todolist

--- structure_models/classes/todo.py
+++ structure_models/classes/todo.py
@@ -4,7 +4,7 @@ import fileinput
 statuslistName = "statuslist.csv"
 todofileName = "todolist.csv"
 
-class toDoItem:
+class ToDoItem:
     def __init__(self, id, description, status, createdon, updatedon):
         self.id = id
         self.description = description
@@ -12,60 +12,45 @@ class toDoItem:
         self.createdon = createdon
         self.updatedon = updatedon
 
-class Status:
-    def __init__(self, status):
-        self.status = status
+class ToDoList:
+    def __init__(self):
         self.listofitems = []
 
-    def loadItems(self):
-        reader = toDoReader()
-        self.listofitems = reader.ReadFromFile(todofileName, self.status)
-
-    def show_my_items(self):
-        print "\nHere is the list of %s things: " % self.status
-        for task in self.listofitems:
-            print "%s, %s. Was created on: %s. Was last updated on: %s." % (task.id, task.description, task.createdon, task.updatedon)
-
-class toDoAppend:
-    def appendItem(self, item, filename):
-        f = open(filename, "a")
-        num_lines = sum(1 for line in open(filename) if line.rstrip())
-        line = num_lines + 1
+    def appendItem(self, item):
+        array = self.listofitems
+        ID = len(array) + 1
         description = item
-        status = "Not_Done"
+        status = "not_done"
         createdon = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
         updatedon = createdon
-        lineToAddIntoFile = "%s,%s,%s,%s,%s" % (line, description, status, createdon, updatedon)
-        f.write(lineToAddIntoFile + '\n')
-        f.flush()
-        f.close()
+        self.listofitems.append(ToDoItem(ID, description, status, createdon, updatedon))
 
-class itemEditor:
-    def edit(self, id, filename):
-        f = open(filename, "a")
-        old = raw_input("What phrase do you want to change?: ")
-        new = raw_input("What do you want to put instead?: ")
-        for line in fileinput.FileInput(filename, inplace=1):
-            if line.startswith(id):
-                line = line.replace(old, new)
-                print line
-        f.flush()
-        f.close()
+    def edit(self, number):
+        for task in self.listofitems:
+            if number == task.id:
+                choice = raw_input("What do you want to change (description or status)?: ")
+                if choice == "description":
+                    new_description = raw_input("Type new description: ")
+                    new_description = new_description.replace (" ", "_")
+                    task.description = new_description
+                if choice == "status":
+                    new_status = raw_input("Type in new status (done or not done): ")
+                    new_status = new_status.replace (" ", "_")
+                    task.status = new_status
+                task.updatedon = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
         
-        
-        # num_lines = sum(1 for line in open(filename) if line.rstrip())
-        # line = num_lines + 1
-        # description = item
-        # status = "Not_Done"
-        # createdon = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        # updatedon = createdon
-        # lineToAddIntoFile = "%s,%s,%s,%s,%s" % (line, description, status, createdon, updatedon)
-        # f.write(lineToAddIntoFile + '\n')
-        # f.flush()
-        # f.close()
+    def loadItems(self):
+        reader = ToDoReader()
+        self.listofitems = reader.ReadFromFile(todofileName, self.status)
+
+    def show_my_items(self, status):
+        print "\nHere is the list of %s things: " % status
+        for task in self.listofitems:
+            if task.status == status:
+                print "%s, %s. Was created on: %s. Was last updated on: %s." % (task.id, task.description, task.createdon, task.updatedon)
 
 
-class toDoReader:
+class ToDoReader:
     def ReadFromFile(self, fileName, status):
         array = []
         f = open(fileName, "r")
@@ -73,60 +58,48 @@ class toDoReader:
             line = line.replace('\n','')
             id, description, stat, createdon, updatedon = line.split(",")
             if status == stat:
-                array.append(toDoItem(id, description, status, createdon, updatedon))
+                array.append(ToDoItem(id, description, status, createdon, updatedon))
         f.close()
         return array
-
-class StatusReader:
-    def ReadFromFile(self, fileName):
-        array = []
-        f = open(fileName, "r")
-        for line in f:
-            line = line.replace('\n','')
-            status = line
-            array.append(Status(status))
-        f.close()
-        return array
-
 
 def main():
-    typeofaction = raw_input("What do you want to do with your list (add, edit, print)? ")
-    if typeofaction == "add":
-        item = raw_input("Type the description: ")
-        item = item.replace (" ", "_")
-        toDoAppend().appendItem(item, todofileName)
-    
-    if typeofaction == "edit":
-        f = open(todofileName, "r")
-        for line in f:
-            print line
-        id = raw_input("Type ID of the item you want to edit: ")
-        f.close()
-        itemEditor().edit(id, todofileName)
+    todolist = ToDoList()
 
+    f = open(todofileName, "r")
+    for line in f:
+        line = line.replace('\n','')
+        id, description, status, createdon, updatedon = line.split(",")
+        item = ToDoItem(id, description, status, createdon, updatedon)
+        todolist.listofitems.append(item)
+    f.close()
 
+    while(True):
+        typeofaction = raw_input("What do you want to do with your list (add, edit, print, exit)? ")
+        if typeofaction == "add":
+            item = raw_input("Type the description: ")
+            item = item.replace (" ", "_")
+            todolist.appendItem(item)
 
+        if typeofaction == "edit":
+            for item in todolist.listofitems:
+                print "%s,%s,%s,%s,%s" % (item.id, item.description, item.status, item.createdon, item.updatedon)
+            number = raw_input("Type ID of the item you want to edit: ")
+            todolist.edit(number)
 
-    if typeofaction == "print":
-        statusReader = StatusReader()
-        todolist = statusReader.ReadFromFile(statuslistName)
-        
-        for item in todolist:
-            item.loadItems()
-
-        for item in todolist:
-            item.show_my_items()
+        if typeofaction == "print":
+            s = raw_input("Items of which status do you want to print (done, not done)?: ")
+            s = s.replace (" ", "_")
+            todolist.show_my_items(s)
+            
+        if typeofaction == "exit":
+            open(todofileName, "w").close()
+            f = open(todofileName, "a")
+            for item in todolist.listofitems:
+                linetoadd = "%s,%s,%s,%s,%s\n" % (item.id, item.description, item.status, item.createdon, item.updatedon)
+                f.write(linetoadd)
+            f.flush()
+            f.close()
+            break
 
 if __name__ == "__main__":
     main()
-
-
-
-
-
-# >>> from time import gmtime, strftime
-# >>> strftime("%Y-%m-%d %H:%M:%S", gmtime())
-# '2009-01-05 22:14:39'
-
-# >>> from datetime import datetime
-# >>> datetime.now().strftime('%Y-%m-%d %H:%M:%S')
